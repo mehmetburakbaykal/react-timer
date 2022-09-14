a5f8524ecd8c243c8b08b8861dc303abb943b14e
Burak Baykal <mehmetburakbaykal1@gmail.com>
2022-09-14 22:51:40 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const date = new Date().toLocaleTimeString();
+
 function App() {
-  return;
+  const [time, setTime] = useState(date);
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+  }, [time]);
+
+  return <div>{time}</div>;
 }
 
 export default App;
